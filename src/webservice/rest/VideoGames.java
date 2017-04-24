7905833931fd11a60fbb3150763ac0c1d14e82aa
Virgile Vançon <virgilew@gmail.com>
2017-04-24 18:40:24 +0200

    REST VideoGames

--- src/webservice/rest/VideoGames.java
+++ src/webservice/rest/VideoGames.java
@@ -3,9 +3,13 @@ package webservice.rest;
 import java.util.List;
 
 import javax.annotation.security.PermitAll;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 
@@ -20,34 +24,23 @@ import dao.GameDao;
 
 
 @Path("/VideoGames")
-//@Consumes(MediaType.APPLICATION_JSON)
-//@Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
 public class VideoGames {
 	
 	@PermitAll
 	@GET
 	@Path("/games")
+	@Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
 	public Response getProducts(@Context HttpRequest request) {
-		
-//		System.out.println("\n!!!!!!!!!!     GetProducts OK      !!!!!!!");
-//		List<Game> list = GameDao.findAllSQL();
-//		for(Game g : list) {
-//			System.out.println(g.getTitle());
-//		}
-//		ResponseBuilder rb;
-//		rb = Response.ok().status(200);
-//		return rb.build();
-		
-		
-		
-		
-		List<Game> list = GameDao.findAll();
+
+		List<Game> dataBaseGames = GameDao.findAllSQL();
 		
 		ObjectMapper mapper = new ObjectMapper();
 		mapper.enable(SerializationFeature.INDENT_OUTPUT);
 		String json = "[]";
 		try {
-			json = mapper.writeValueAsString(list);
+			json = mapper.writeValueAsString(dataBaseGames);
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
 		}
@@ -61,5 +54,37 @@ public class VideoGames {
 		}		
 		return rb.build();
 	}
+	
+	@PermitAll
+	@GET
+	@Path("/games/{name}")
+	@Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+	public Response getVideoGame(@PathParam("name") String name) {
+		Game game = GameDao.findSQL(name);
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.enable(SerializationFeature.INDENT_OUTPUT);
+		String json = "[]";
+		try {
+			json = mapper.writeValueAsString(game);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+
+		ResponseBuilder rb;
+		if (json.isEmpty()) {
+			rb = Response.serverError().status(404);
+		} else {
+			rb = Response.ok(json).status(200);
+		}
+		return rb.build();
+	}
+	
+//	@PermitAll
+//	@GET
+//	@Path("/games/{name}/type")
+//	@Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+//	public Response getVideoGameType(@PathParam("name") String name) {
+//		
+//	}
 
 }

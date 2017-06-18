329b89f9608f44c6562ecde37246d214463b6f7f
Virgile Vançon <virgilew@gmail.com>
2017-06-18 15:30:21 +0200

    corrections import inutiles

--- src/controllers/Inscription.java
+++ src/controllers/Inscription.java
@@ -1,11 +1,6 @@
 package controllers;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
